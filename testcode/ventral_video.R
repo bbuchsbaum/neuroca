2e9e0adad8ae3149bcfaa8bc36176c969cbe6af1
Bradley Buchsbaum <bbuchsbaum@Donacobius.local>
2017-05-24 18:08:30 -0400

    yolo

--- testcode/ventral_video.R
+++ testcode/ventral_video.R
@@ -12,12 +12,11 @@ load_mat <- function(fname) {
 }
 
   
-  
 
 vdat <- lapply(sids, function(sid) {
-  v1 <- load_mat(paste0(sid, "_ventral_video_1.RDS"))
-  v2 <- load_mat(paste0(sid, "_ventral_video_2.RDS"))
-  v3 <- load_mat(paste0(sid, "_ventral_video_3.RDS"))
+  v1 <- load_mat(paste0(sid, "_s5_ventral_video_1.RDS"))
+  v2 <- load_mat(paste0(sid, "_s5_ventral_video_2.RDS"))
+  v3 <- load_mat(paste0(sid, "_s5_ventral_video_3.RDS"))
   des <- rbind(v1$design, v2$design, v3$design)
   des$run <- factor(rep(1:3, each=364))
   
@@ -38,7 +37,7 @@ vdat <- lapply(sids, function(sid) {
 
 ndat <- lapply(vdat, function(vd) {
   sid <- vd$sid
-  v1 <- load_mat(paste0(sid, "_ventral_nback.RDS"))
+  v1 <- load_mat(paste0(sid, "_s5_ventral_nback.RDS"))
   v1$design$LabelVersion <- factor(paste0(as.character(v1$design$Label), "_", v1$design$Version))
   keep <- vd$keep
   mat <- v1$mat[, keep]
@@ -73,6 +72,25 @@ ndat <- lapply(vdat, function(vd) {
   list(sid=sid, mat=mat_miss, design=des, ncomp=ncomp, keep=keep, len=ncol(mat))
 })
 
+
+
+idat <- lapply(vdat, function(vd) {
+  sid <- vd$sid
+  v1 <- load_mat(paste0(sid, "_s5_ventral_imagery.RDS"))
+  v1$design$LabelVersion <- factor(paste0(as.character(v1$design$label), "_", v1$design$version))
+  keep <- vd$keep
+  mat <- v1$mat[, keep]
+  
+  rmat <- resid(lsfit(model.matrix(~ factor(v1$design$run)), mat, intercept=FALSE))
+  mat <- t(scale(t(rmat)))
+  list(sid=sid, mat=mat, design=v1$design, keep=keep, len=ncol(mat))
+  
+})
+
+
+
+
+library(softImpute)
 Xnback <- do.call(cbind, lapply(ndat, "[[", "mat"))
 imp_fit <- softImpute(Xnback, rank.max=25, type="als", trace.it=TRUE)
 Xcomplete <- complete(Xnback,imp_fit)
@@ -108,7 +126,7 @@ Y <- factor(c(zerostr(1:1092), as.character(ndat[[1]]$design$label)))
 nback_ind <- 1093:(1093+179)
 
 mbres <- musubada(Y, Xl, ncomp=20, normalization="MFA", rank_k=200)
-
+library(ggrepel)
 df1 <- data.frame(pc1=mbres$scores[nback_ind,1], pc2=mbres$scores[nback_ind,2], pc3=mbres$scores[nback_ind,3], 
                   pc4=mbres$scores[nback_ind,4], pc5=mbres$scores[nback_ind,5], labels=row.names(mbres$scores)[nback_ind])
 
@@ -144,21 +162,34 @@ preds <- unlist(lapply(levels(Ysubj), function(sid) {
   train_idx <- which(Ysubj != sid)
   test_idx <- which(Ysubj == sid)
   sda.r <- sda.ranking(Xcat[train_idx,], Ynback2[train_idx], ranking.score="avg", verbose=FALSE)
-  keep <- which(sda.r[, "score"] > 0)
+  keep <- which(sda.r[, "score"] > 2)
   keepid <- sda.r[, "idx"]
   keepid <- keepid[keep]
   
   sda.1 <- sda(Xcat[train_idx,keepid], Ynback2[train_idx], verbose=FALSE)
+  ##rf.1 <- randomForest(Xcat[train_idx,keepid], Ynback2[train_idx], ntree=1500, mtry=8)
   p <- predict(sda.1, Xcat[test_idx,keepid], verbose=FALSE)
   print( sum(p$class == Ynback2[test_idx])/length(test_idx) * 100)
   p$class
 
 }))
 
+Ximagery <- do.call(cbind, lapply(idat, "[[", "mat"))
+Yimagery <- idat[[1]]
+
+Xnback <- do.call(cbind, lapply(ndat, "[[", "mat"))
+imp_fit <- softImpute(Xnback, rank.max=50, type="als", trace.it=TRUE)
+Xcomplete <- complete(Xnback,imp_fit)
+lens <- lapply(ndat, "[[", "len")
+groups <- rep(1:length(lens), unlist(lens))
+
+Xbm <- matrix_to_block_matrix(Xcomplete, groups)
+Xl <- as.list(Xbm)
+Y <- ndat[[1]]$design$label
+mbres <- musubada(Y, Xl, ncomp=10, normalization="MFA")
 
 
-
-
+Xibm <- as.list(matrix_to_block_matrix(Ximagery, groups))
 
 
 
