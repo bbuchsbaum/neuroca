6e9d5da2b82e7f8a2375e04eb9dc0901e75eb08f
Bradley Buchsbaum <bbuchsbaum@Donacobius.local>
2017-06-28 19:53:30 -0400

    yolo

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -39,12 +39,15 @@ bootstrap.musu_bada <- function(x, niter, ncomp=x$ncomp) {
   }
   
   do_boot <- function() {
+    ## resample original data with replacement, generating a new dataset
     resam <- resample.musu_bada(x)
+    
+    ## calculate barycenters of pre-processed data
     XBary <- do.call(cbind, lapply(1:length(resam$X), function(i) {
       group_means(resam$Y[[i]], x$reprocess(resam$X[[i]], i))
     }))
     
-    
+    ## project rows and columns of barycenters
     br <- project.rows(XBary)
     bc <- project.cols(XBary)
     
@@ -76,58 +79,3 @@ bootstrap.musu_bada <- function(x, niter, ncomp=x$ncomp) {
   
 }
 
-
-# 
-# bootstrap.musubada_result <- function(x, niter) {
-#   
-# }
-# 
-# 
-# boot_ratio <- function(bootlist) {
-#   boot.mean <- Reduce("+", bootlist)/length(bootlist)
-#   boot.sd <- sqrt(Reduce("+", lapply(bootlist, function(mat) (mat - boot.mean)^2))/length(bootlist))
-#   boot.mean/boot.sd	
-# }
-# 
-# .resample <- function(x, ...)  { x[sample.int(length(x), ...)] }
-# 
-# sample_indices <- function(Y) {
-#   sort(unlist(lapply(levels(x$Y), function(lev) {
-#     sample(which(x$Y == lev), replace=TRUE)
-#   })))
-# }
-# 
-# boot.res <- 
-#   lapply(1:niter, function(i) {
-#     message("bootstrap iteration: ", i)
-#     if (is.null(x$strata)) {
-#       row_indices <- sample_indices(x$Y)
-#       do_boot(sort(unlist(row_indices)))
-#     } else {
-#       levs <- levels(x$strata)
-#       slevs <- sample(levs, length(levs), replace=TRUE)
-#       ind <- sort(sapply(slevs, function(lev) which(lev == levs)))
-#       ret <- do_stratified_boot(ind)
-#     }
-#   })
-# 
-# 
-# do_boot <- function(indices) {
-#   XBoot <- x$X[indices,]
-#   YBoot <- x$Y[indices]
-#   
-#   #Xc <- x$pre_process(XBoot)
-#   
-#   ## barycenters of bootstrap samples
-#   #XB <- x$reduce(XBoot, YBoot)
-#   XB <- group_means(YBoot, XBoot)
-#   
-#   ## apply pre-processing from full model
-#   XBc <- x$pre_process(XB) 
-#   
-#   br <- project.rows(XBc, x$u)
-#   bc <- project.cols(t(XBc), x$v)
-#   
-#   list(boot4R=br,
-#        boot4C=bc)
-# }
