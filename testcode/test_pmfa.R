7506c0adce7dfb45c1d0c9a21a99bfe883f77ea1
Bradley Buchsbaum <bbuchsbaum@Donacobius.local>
2017-09-25 09:32:45 -0400

    added procrusteanize, to_block_matrix, genpca fix, musu_bada now uses genpca
    Former-commit-id: 84301fb375d41c1e692d51fe86bdbfb85b379487

---
+++ testcode/test_pmfa.R
@@ -0,0 +1,143 @@
+library(missMDA)
+
+
+zerostr <- function(vals) {
+  ndigits <- log(max(vals), 10) + 1
+  nzeros <- ndigits - as.integer(log(vals,10)) -1
+  prefix <- sapply(nzeros, function(nz) paste(rep("0", times=nz), collapse=""))
+  paste(prefix, vals, sep="")  
+}
+
+
+alldat <- readRDS("/Users/bbuchsbaum/analysis/hyper/ventral_surface/ROI_11121_alldat.RDS")
+
+vmatlist <- lapply(alldat$vdat, function(x) x$mat)
+ndatlist <- lapply(alldat$ndat, function(x) x$mat)
+idatlist <- lapply(alldat$idat, function(x) x)
+
+bothmat <- do.call(cbind, lapply(1:length(vmatlist), function(i) rbind(vmatlist[[i]], ndatlist[[i]])))
+group <- sapply(vmatlist, ncol)
+
+both_imp <- imputeMFA(as.data.frame(bothmat), group, ncp=10, method="EM", maxiter=100, threshold = 1e-05)
+both_mat <- to_block_matrix(as.matrix(both_imp$completeObs), group)
+both_xlist <- as.list(both_mat)
+both_xlist <- lapply(both_xlist, function(x) t(scale(t(x))))
+
+Y <- factor(c(zerostr(1:nrow(vmatlist[[1]])), as.character(alldat$ndat[[1]]$design$label)))
+mres <- musu_bada(Y, both_xlist, normalization="MFA")
+proc_mres <- procrusteanize.musu_bada(mres, ncomp=10)
+
+
+project_musu <- function(mfit, idatlist) {
+  nback_ind <- 274:453
+  
+  tvals <- sort(unique(idatlist[[1]]$design$time))
+
+  do.call(rbind, lapply(1:length(tvals), function(tind) {
+    Xim <- lapply(idatlist, function(x) x$spmat[[tind]])
+  
+    pres_cos <- lapply(1:length(Xim), function(i) {
+      print(i)
+      pred <- predict(mfit, Xim[[i]], type="cosine", ncomp=mfit$ncomp, table_index=i)
+      pred <- pred[,nback_ind]
+  
+    
+      des <- subset(idatlist[[i]]$design, time==tvals[tind])
+      lver <- des$LabelVersion
+      ind <- match(as.character(lver), mres$Y[[1]][nback_ind])
+  
+      react <- pred[cbind(1:length(lver), ind)]
+    
+      other_version <- ifelse(des$version == 1, 2, 1)
+      other_label <- paste0(des$label, "_", other_version)
+      ind_other <- match(as.character(other_label), mfit$musufit$Y[[1]][nback_ind])
+    
+      react_other <- pred[cbind(1:length(lver), ind_other)]
+    
+    
+      dfx1 <- do.call(rbind, replicate(3, data.frame(time = tvals[tind],
+                       cond = ifelse(des$cresp == 1, "same", "different"),
+                       acc = des$acc,
+                       zvivid = scale(des$vivid),
+                       zconf = scale(des$confidence),
+                       sid=sids[i],
+                       label=des$label,
+                       label_version=des$LabelVersion), simplify=FALSE))
+    
+      dfx1$type <- rep(c("react", "react_other", "react_diff"), each=nrow(dfx1)/3)
+      dfx1$react <- c(react, react_other, react-react_other)
+      dfx1
+    })
+  
+    do.call(rbind, pres_cos)
+  }))
+}
+
+pres_cos = project_musu(proc_mres, idatlist)
+library(dplyr)
+library(ggplot2)
+
+
+# 
+reac_by_time <- pres_cos %>% group_by(time, type) %>% summarize(react=mean(react, na.rm=TRUE))
+                                                       
+qplot(time, react, data=reac_by_time, geom=c("point", "line")) + facet_wrap( ~ type)
+# 
+reac_by_acc <- pres_cos %>% group_by(time, acc, type) %>% summarize(react=mean(react, na.rm=TRUE))
+qplot(time, react, colour=factor(acc), data=reac_by_acc, geom=c("point", "line")) + facet_wrap( ~ type)
+# 
+# 
+reac_by_viv <- pres_cos %>% group_by(sid) %>% mutate(qvivid=ntile(zvivid, 3)) %>% ungroup() %>% group_by(time, qvivid, type) %>% 
+   summarize(react=mean(react, na.rm=TRUE))
+#   
+qplot(time, react, colour=factor(qvivid), data=subset(reac_by_viv, !is.na(qvivid)), geom=c("point", "line")) + facet_wrap( ~ type)
+
+reac_by_viv_cond <- pres_cos %>% group_by(sid) %>% mutate(qvivid=ntile(zvivid, 3)) %>% ungroup() %>% group_by(time, qvivid, cond, type) %>% 
+summarize(react=mean(react, na.rm=TRUE))
+ 
+qplot(time, react, colour=factor(qvivid), data=subset(reac_by_viv_cond, !is.na(qvivid)), geom=c("point", "line")) + facet_wrap(cond ~ type)
+# 
+# 
+reac_by_viv_acc <- pres_cos %>% group_by(sid) %>% mutate(qvivid=ntile(zvivid, 3)) %>% ungroup() %>% group_by(time, qvivid, acc, type) %>% 
+   summarize(react=mean(react, na.rm=TRUE))
+ 
+qplot(time, react, colour=factor(qvivid), data=subset(reac_by_viv_acc, !is.na(qvivid) & acc==1), geom=c("point", "line")) + 
+  facet_wrap( ~ type)
+
+reac_by_cond <- pres_cos %>%  group_by(time, cond, type) %>% summarize(react=mean(react, na.rm=TRUE)) 
+qplot(time, react, colour=factor(cond), data=reac_by_cond, geom=c("point", "line")) + facet_wrap(~ type)
+
+
+reac_by_cond_acc <- pres_cos %>%  group_by(time, cond,acc,type) %>% summarize(react=mean(react, na.rm=TRUE))
+qplot(time, react, colour=factor(acc), facets = . ~ cond, data=reac_by_cond_acc, geom=c("point", "line")) + facet_wrap(cond ~ type)
+
+reac_by_conf <- pres_cos %>%  group_by(sid) %>% mutate(qconf=ntile(zconf, 3)) %>% ungroup() %>% group_by(time, qconf, type) %>% 
+summarize(react=mean(react, na.rm=TRUE))
+
+qplot(time, react, colour=factor(qconf), data=subset(reac_by_conf, !is.na(qconf)), geom=c("point", "line")) + facet_wrap(~ type)
+
+reac_by_conf_cond <- pres_cos %>% group_by(sid) %>% mutate(qconf=ntile(zconf, 3)) %>% ungroup()  %>% group_by(time, qconf, cond, type) %>% 
+   summarize(react=mean(react, na.rm=TRUE))
+ 
+qplot(time, react, colour=factor(qconf), facets = . ~ cond, data=subset(reac_by_conf_cond, !is.na(qconf)), geom=c("point", "line")) +
+   facet_wrap(cond ~ type)
+ 
+reac_by_conf_acc <- pres_cos %>%  group_by(sid) %>% mutate(qconf=ntile(zconf, 3)) %>% ungroup() %>% group_by(time, qconf, acc, type) %>% 
+   summarize(react=mean(react, na.rm=TRUE))
+ 
+qplot(time, react, colour=factor(qconf), data=subset(reac_by_conf_acc, !is.na(qconf)), geom=c("point", "line")) +
+facet_wrap(acc ~ type)
+ 
+reac_by_viv_conf <- pres_cos %>% group_by(sid) %>% mutate(qvivid=ntile(zvivid, 3), qconf=ntile(zconf, 3)) %>% ungroup() %>% 
+   group_by(time, qvivid, qconf, type) %>% 
+   summarize(react=mean(react, na.rm=TRUE))
+
+pres_cos$zmult <- (pres_cos$zvivid - min(pres_cos$zvivid,na.rm=TRUE)) * (pres_cos$zconf - min(pres_cos$zconf, na.rm=TRUE))
+reac_by_qmult <- pres_cos %>% group_by(sid) %>% mutate(qmult=ntile(zmult, 3)) %>% ungroup() %>% 
+  group_by(time, qmult, type,acc) %>% 
+  summarize(react=mean(react, na.rm=TRUE))
+
+
+ qplot(time, react, colour=factor(qmult), data=subset(reac_by_qmult, !is.na(qmult)), geom=c("point", "line")) + 
+   facet_wrap(acc ~ type)
+
