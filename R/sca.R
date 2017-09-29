6010cef56755199d094037bec2afd19b3751a03e
Bradley Buchsbaum <bbuchsbaum@Donacobius.local>
2017-09-29 16:39:29 -0400

    yolo
    Former-commit-id: 1b860e24ad9fdc290cd472422d89ecd31a878871

---
+++ R/sca.R
@@ -0,0 +1,46 @@
+
+#' @importFrom multiway sca
+sca <- function(X, ncomp=2, center=TRUE, scale=FALSE, rank_k=NULL,
+                type=c("sca-p","sca-pf2","sca-ind","sca-ecp"), ...)
+  
+  assertthat::assert_that(inherits(X, "block_matrix"))
+  type <- match.arg(type)
+  
+  X <- pre_processor(X, center=center,scale=scale)
+  
+  Xr <- if (!is.null(rank_k)) {
+    is_reduced <- TRUE
+    reducer <- reduce_rank(X, rank_k)
+    reducer$x
+  } else {
+    is_reduced=FALSE
+    reducer <- NULL
+    X
+  }
+  
+  Xl <- as.list(block_apply(Xr, t))
+  
+  sca_fit <- sca(Xl, nfac=ncomp, type=type, ...)
+  
+  reprocess <- function(newdat, table_index) {
+    prep <- attr(X, "pre_process")
+    newdat <- prep(newdat, bind[[table_index]])
+    
+    if (is_reduced) {
+      newdat <- project(reducer, newdat, table_index)
+    }
+    
+    newdat
+  }
+  
+  ret <- list(
+    sca_fit=sca_fit,
+    reprocess=reprocess,
+    center=center,
+    scale=scale,
+    rank_k=rank_k,
+    is_reduced=is_reduced)
+  
+  class(ret) <- c("sca", "list")
+    
+}
