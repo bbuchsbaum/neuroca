974522cafbdda68a72c55e115aaca8dedacde9c5
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2017-10-13 00:39:17 -0400

    more mfa tests

--- tests/testthat/test_mfa.R
+++ tests/testthat/test_mfa.R
@@ -23,3 +23,13 @@ test_that("can project a row vector", {
   expect_equal(nrow(proj), 1)
 })
 
+test_that("can project a matrix for a single table", {
+  res1 <- mfa(block_mat, center=TRUE, ncomp=5, normalization="None")
+  x <- matrix(rnorm(10*10), 10, 10)
+  proj <- project(res1, x, comp=1:3, table_index=1)
+  expect_equal(ncol(proj), 3)
+  expect_equal(nrow(proj), nrow(x))
+})
+
+
+
