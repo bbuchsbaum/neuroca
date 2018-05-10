34f039bd4b8edd51674f84868030a2b895a05cd7
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2018-05-10 07:35:37 -0400

    pca composition

--- R/pca.R
+++ R/pca.R
@@ -11,13 +11,10 @@
 #' @importFrom denoiseR adashrink
 #' @export
 shrink_pca <- function(X, center=TRUE, scale=FALSE,  method = c("GSURE", "QUT", "SURE"), ...) {
-  xdim <- dim(X)
-  proj_fun <- projection_fun(X)
-  
   preproc <- pre_processor(X, center, scale)
   Xp <- pre_process(preproc, X)
   
-  res <- adashrink(Xp, method=method, center=FALSE, ...)
+  res <- denoiseR::adashrink(Xp, method=method, center=FALSE, ...)
   
   keep <- res$singval > 1e-06
   
@@ -33,9 +30,7 @@ shrink_pca <- function(X, center=TRUE, scale=FALSE,  method = c("GSURE", "QUT", 
     d=res$low.rank$d[keep]
   }
   
-  ret <- list(proj_fun=proj_fun,
-              xdim=xdim,
-              v=v, 
+  ret <- list(v=v, 
               u=u,
               d=d,
               scores=t(t(as.matrix(u)) * d),
@@ -65,9 +60,7 @@ pseudo_pca <- function(u, v, d, rnames=NULL) {
   
   assert_that(length(d) == nrow(u))
   
-  ret <- list(xdim=c(length(d), nrow(v)),
-              proj_fun=identity,
-              v=v, 
+  ret <- list(v=v, 
               u=u, 
               d=d, 
               scores=scores, 
@@ -87,9 +80,6 @@ pseudo_pca <- function(u, v, d, rnames=NULL) {
 #' @export
 pca <- function(X, ncomp=min(dim(X)), center=TRUE, scale=FALSE, ...) {
   
-  xdim <- dim(X)
-  proj_fun <- projection_fun(X)
-  
   preproc <- pre_processor(X, center=center,scale=scale)
   Xp <- pre_process(preproc)
   
@@ -101,9 +91,7 @@ pca <- function(X, ncomp=min(dim(X)), center=TRUE, scale=FALSE, ...) {
     row.names(scores) <- row.names(Xp)[seq_along(svdres$d)]
   }
   
-  ret <- list(xdim=xdim,
-              proj_fun=proj_fun,
-              v=svdres$v, 
+  ret <- list(v=svdres$v, 
               u=svdres$u, 
               d=svdres$d, 
               scores=scores, 
@@ -121,10 +109,7 @@ reprocess.pca <- function(x, newdata, subind=NULL) {
   if (is.null(subind)) {
     assert_that(ncol(newdata) == ncol(x))
     pre_process(x$preproc, newdata)
-    #xp <- x$proj_fun(newdata)
-    #x$pre_process(xp)
   } else {
-    #browser()
     assert_that(length(subind) == ncol(newdata), 
                 msg=paste("length of subind not equal to number of columns of newdata", length(subind), "!=", ncol(newdata)))
     pre_process(x$preproc, newdata, subind)
@@ -154,17 +139,13 @@ projection_fun.pca <- function(x, comp=1:ncomp(x), pre_process=TRUE) {
   }
 }
 
-#' @export
-inverse_project.pca <- function(x, newdata, comp=1:ncomp(x), subind=NULL) {
-  reverse_pre_process(x$preproc, newdata %*% t(loadings(x)[,comp], subind))
-}
 
 #' @export
 project.pca <- function(x, newdata, comp=1:ncomp(x), pre_process=TRUE, subind=NULL) {
   
   ## if no newdata, then simply return the factor scores
   if (missing(newdata)) {
-    return(scores(x)[,comp])
+    return(scores(x)[,comp, drop=FALSE])
   }
   
   if (is.vector(newdata)) {
@@ -172,43 +153,42 @@ project.pca <- function(x, newdata, comp=1:ncomp(x), pre_process=TRUE, subind=NU
   }
   
   if (is.null(subind)) {
-    reprocess(x, newdata) %*% loadings(x)[,comp]
+    reprocess(x, newdata) %*% loadings(x)[,comp, drop=FALSE]
   } else {
     ## subind must be in the input space
     assertthat::assert_that(max(subind) <= ncol(newdata))
-    reprocess(x, newdata, subind=subind) %*% (loadings(x)[subind, comp])
+    reprocess(x, newdata, subind=subind) %*% (loadings(x)[subind, comp, drop=FALSE])
   }
 }
-
 
 #' @export
 residuals.pca <- function(x, ncomp=1, xorig) {
-  recon <- scores(x)[,1:ncomp,drop=FALSE] %*% t(loadings(x)[,1:ncomp,drop=FALSE])
-  newdat <- pre_process(x$preproc, xorig)
-  newdat - recon
+  recon <- reconstruct(x,comp=1:ncomp)
+  #recon <- scores(x)[,1:ncomp,drop=FALSE] %*% t(loadings(x)[,1:ncomp,drop=FALSE])
+  orig <- pre_process(x$preproc, xorig)
+  orig - recon
 }
 
 
 #' @export
-reconstruct.pca <- function(x, newdata=NULL, comp=1:x$ncomp, subind=NULL) {
-  if (is.null(newdata)) {
-    ## reconstruct and then uncenter_scale
-    reverse_pre_process(x$preproc, scores(x)[,comp,drop=FALSE] %*% t(loadings(x)[,comp,drop=FALSE]), subind=subind)
+reconstruct.pca <- function(x, newdata=NULL, comp=1:x$ncomp) {
+  if (!is.null(newdata)) {
+    assert_that(ncol(newdata) == length(comp) && nrow(newdata) == nrow(scores(x)))
   } else {
-    ## newdata must have length(comp) columns
-    reverse_pre_process(x$preproc, newdata %*% t(loadings(x)[,comp,drop=FALSE]), subind=subind)
+    newdata <- scores(x)[,comp, drop=FALSE]
   }
+  reverse_pre_process(x$preproc, newdata %*% t(loadings(x)[,comp,drop=FALSE]))
 }
 
 
 #' @export
 ncol.pca <- function(x) {
-  x$xdim[2]
+  nrow(x$v)
 }
 
 #' @export
 nrow.pca <- function(x) {
-  x$xdim[1]
+  nrow(x$u)
 }
 
 #' @export
@@ -228,9 +208,7 @@ truncate.pca <- function(obj, ncomp) {
     warning("number of components to keep is greater than or equal to rank of pca fit, returning original model")
     ret <- obj
   } else {
-    ret <- list(xdim=obj$xdim,
-                proj_fun=obj$proj_fun,
-                v=obj$v[,1:ncomp,drop=FALSE], 
+    ret <- list(v=obj$v[,1:ncomp,drop=FALSE], 
                 u=obj$u[,1:ncomp,drop=FALSE], 
                 d=obj$d[1:ncomp], 
                 scores=obj$scores[,1:ncomp,drop=FALSE], 
@@ -260,10 +238,3 @@ reduce_rank.matrix <- function(x, k=min(dim(x)), center=TRUE, scale=FALSE,
   res <- reducer(x, k, center=center, scale=scale, ...)
 }
 
-
-
-
-
-# 
-# hierarchical_pca <- function(X, comps, hierarchy, svd.method="base") {
-# }
--- R/projection.R
+++ R/projection.R
@@ -23,9 +23,6 @@ is_transformer.matrix <- function(x) FALSE
 is_transformer.Matrix <- function(x) FALSE
 
 
-
-
-
 #' @export
 predict.projector <- function(x, newdata, ncomp=ncomp(x)) {
   project(x, newdata, comp=1:ncomp)
@@ -112,5 +109,84 @@ project.block_matrix <- function(x, newdata=NULL) {
   #  as.matrix(newdata)
   #}
 }
+
+compose_all <- function(...) {
+  args <- list(...)
+  assert_that(length(args) > 1)
+  
+  out <- compose.projector(args[[1]], args[[2]])
+  if (length(args) > 2) {
+    for (i in 3:length(args)) {
+      out <- compose.projector(out, args[[i]])
+    }
+  }
+  
+  out
+}
+
+
+
+
+#' @export 
+compose.projector <- function(x,y) {
+  assert_that(inherits(y, "projector"), msg=paste("y does not inherit from class 'projector': ", class(y)))
+  
+  proj <- function(newdata) {
+    project(y, project(x, newdata))
+  }
+  recon <- function(newdata=NULL) {
+    reconstruct(x, reconstruct(y, newdata))
+  }
+  
+  structure(
+    list(
+      a=x,
+      b=y,
+      d=y$d,
+      u=y$u,
+      xdim=dim(x),
+      ncomp=ncomp(y),   
+      proj=proj,
+      scores=scores(y),
+      recon=recon),
+    class=c("composed_projector", "projector")
+  )
+}
+
+#' @export
+loadings.composed_projector <- function(x) {
+  t(x$u %*% diag(1/x$d)) %*% reconstruct(x)
+}
+
+#' @export
+scores.composed_projector <- function(x) { x$scores }
+
+#' @export
+dim.composed_projector <- function(x) c(nrow(x), ncol(x))
+
+#' @export
+ncol.composed_projector <- function(x) x$xdim[2]
+
+#' @export
+nrow.composed_projector <- function(x) x$xdim[1]
+
+#' @export
+ncomp.composed_projector <- function(x) ncol(scores(x))
+
+#' @export
+project.composed_projector <- function(x, newdata=NULL) {
+  if (is.null(newdata)) {
+    scores(x)
+  } else {
+    x$proj(newdata)
+  }
+}
+
+#' @export
+reconstruct.composed_projector <- function(x, newdata=NULL) {
+  x$recon(newdata)
+}
+
+
 
 
