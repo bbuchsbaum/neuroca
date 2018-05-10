8cedb960aafce51075d12b00786b366e340d2a58
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2018-05-10 07:35:37 -0400

    pca composition
    Former-commit-id: 34f039bd4b8edd51674f84868030a2b895a05cd7

--- tests/testthat/test_mfa.R
+++ tests/testthat/test_mfa.R
@@ -31,15 +31,15 @@ test_that("can project a matrix for a single table", {
   expect_equal(nrow(proj), nrow(x))
 })
 
-test_that("can run mfa with a block_projection_matrix", {
-  bm <- block_projection_matrix(lapply(1:5, function(i) pca(matrix(rnorm(10*10), 10, 10), ncomp=4)))
+test_that("can run mfa on pca scores", {
+  bm <- block_projector(lapply(1:5, function(i) pca(matrix(rnorm(10*10), 10, 10), ncomp=4)))
   
-  res1 <- mfa(bm, center=TRUE, ncomp=5, normalization="None")
+  res1 <- mfa(project(bm), center=TRUE, ncomp=5, normalization="None")
   
-  x <- matrix(rnorm(10*10), 10, 10)
+  x <- matrix(rnorm(10*10), 10, 4)
   proj <- project(res1, x, comp=1:5, block_index=1)
   
-  x2 <- matrix(rnorm(10*50), 10, 50)
+  x2 <- matrix(rnorm(10*20), 10, 20)
   proj_all  <- project(res1, x2)
   
   expect_equal(ncol(proj), 5)
@@ -48,21 +48,20 @@ test_that("can run mfa with a block_projection_matrix", {
   expect_equal(ncol(proj_all), 5)
 })
 
-test_that("can run mfa with a block_projection_matrix with matrices with different number of columns", {
-  bm <- block_projection_matrix(lapply(8:12, function(i) pca(matrix(rnorm(10*i), 10, i), ncomp=4)))
+test_that("can run mfa on pca scores and compose with block_projector", {
+  bm <- block_projector(lapply(1:5, function(i) pca(matrix(rnorm(10*10), 10, 10), ncomp=4)))
   
-  res1 <- mfa(bm, center=TRUE, ncomp=5, normalization="None")
+  res1 <- mfa(project(bm), center=TRUE, ncomp=5, normalization="None")
   
-  x <- matrix(rnorm(10*8), 10, 8)
+  x <- matrix(rnorm(10*10), 10, 4)
   proj <- project(res1, x, comp=1:5, block_index=1)
   
-  x2 <- matrix(rnorm(10*sum(8:12)), 10, sum(8:12))
-  proj_all  <- project(res1, x2)
-  
-  expect_equal(ncol(proj), 5)
-  expect_equal(nrow(proj), nrow(x))
-  
-  expect_equal(ncol(proj_all), 5)
+})
+
+test_that("can run mfa with a block_projection_matrix with matrices with different number of columns", {
+  bm <- block_projector(lapply(8:12, function(i) pca(matrix(rnorm(10*i), 10, i), ncomp=4)))
+  res1 <- mfa(bm, center=TRUE, ncomp=5, normalization="None")
+  cp <- compose(bm, res1)
 })
 
 
