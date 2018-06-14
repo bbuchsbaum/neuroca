a82336548e62953a73d7da37d3e9929d9be4da19
Bradley Buchsbaum <bbuchsbaum@Donacobius-9.local>
2018-06-14 11:00:24 -0400

    added constrints file
    Former-commit-id: 9c7dcc3f4f86bdd6f25b8a0a46f5ad703a5eda6d

--- R/constraints.R
+++ R/constraints.R
@@ -1,45 +1,22 @@
-#' compute a spatial contraint matrix that can span across blocks
-#' 
-#' @param a matrix of coordinates for one block that must replicate exactly across \code{nblocks} blocks
-#' @param feature_scores a vector of feature weights used to weight each individual feature
-#' @param sigma the within block smoothing radius
-#' @param sigma_between the across block smoothing radius
-#' @param shrinkage_factor the amount to weight the across block smoothing matrix (heiger leads to more shrinkae)
-#' @export
-spatial_constraints <- function(coords, nblocks=1, feature_scores=rep(1, nrow(coords)*nblocks), 
-                                                                  sigma=5, sigma_between=5, 
-                                                                  shrinkage_factor=.1) {
+
+
+spatial_constraints <- function(coords, nblocks=1, sigma=5, shrinkage_factor=.1, shrinkage_radius=1, global_weights=rep(1, ncol(coords)*nblocks)) {
+  cds2 <- do.call(rbind, lapply(nblocks, function(i) cbind(coords, i*50)))
   
-  Wg <- Diagonal(x=sqrt(feature_scores))   
+  Sw <- neighborweights::spatial_smoother(coords, sigma=sigma, nnk=27, stochastic = TRUE)
+  Swithin <- neighborweights::spatial_smoother(cds2, sigma=sigma, nnk=27, stochastic = TRUE)
+  Matrix::bdiag(as.list(rep(Sw, nblocks))
   
-  if (nblocks > 1 && sigma_between > 0) {
-    print(paste("between smoothing, sigma = ", sigma_between))
-    cds2 <- do.call(rbind, lapply(1:nblocks, function(i) cbind(coords, i*50)))
-    ## within subject spatial smoother
-    Swithin <- neighborweights::spatial_smoother(cds2,sigma=sigma,nnk=27,stochastic = TRUE)
-    
-    cds3 <- do.call(rbind, lapply(1:nblocks, function(i) coords))
-    ## between subject spatial smoother
-    Sbetween <- neighborweights::spatial_adjacency(cds3,weight_mode="binary", 
-                                                 normalize=FALSE,
-                                                 sigma=sigma_between,
-                                                 dthresh=sigma_between*2,
-                                                 nnk=27*nblocks)
+  Sbetween <- neighborweights::spatial_adjacency(as.matrix(indices),weight_mode="binary", 
+                                                 normalize=FALSE,dthresh=bdthresh, include_diagonal=FALSE)/length(perc_dat)
+  diag(Sbetween) <- 0
   
-    Sbetween <- Sbetween/(RSpectra::eigs_sym(Sbetween, k=1, which="LA")$values[1])
+  Wg <- Diagonal(x=sqrt(wg))
   
-    
-    S <- Wg %*% Swithin %*% Wg
-    S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
-    S <- S + Sbetween*shrinkage_factor
-    S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
+  S <- Wg %*% Swithin %*% Wg
+  S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
+  S <- S + Sbetween*bw
+  S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
   
-  } else {
-    print("within smoothing only")
-    Swithin <- Matrix::bdiag(replicate(nblocks, neighborweights::spatial_smoother(coords,sigma=sigma,nnk=27,stochastic = TRUE), simplify=FALSE))
-    S <- Wg %*% Swithin %*% Wg
-    S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
-    S
-  }
-  
+ 
 }
