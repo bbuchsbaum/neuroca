5f7492580d08aed3009e254980fa82dad9954055
Bradley Buchsbaum <bbuchsbaum@Donacobius-9.local>
2018-06-20 16:57:13 -0400

    yolo
    Former-commit-id: fcd703693384e2ae0e4b41999ec6d541fb961221

--- R/constraints.R
+++ R/constraints.R
@@ -5,7 +5,7 @@ spatial_constraints <- function(coords, nblocks=1, sigma=5, shrinkage_factor=.1,
   
   Sw <- neighborweights::spatial_smoother(coords, sigma=sigma, nnk=27, stochastic = TRUE)
   Swithin <- neighborweights::spatial_smoother(cds2, sigma=sigma, nnk=27, stochastic = TRUE)
-  Matrix::bdiag(as.list(rep(Sw, nblocks))
+  #Matrix::bdiag(as.list(rep(Sw, nblocks))
   
   Sbetween <- neighborweights::spatial_adjacency(as.matrix(indices),weight_mode="binary", 
                                                  normalize=FALSE,dthresh=bdthresh, include_diagonal=FALSE)/length(perc_dat)
