50a2543b260d72d6911e114ac5fae136b61aae6d
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2018-09-17 19:52:33 -0400

    docs

--- R/constraints.R
+++ R/constraints.R
@@ -26,14 +26,14 @@ spatial_constraints <- function(coords, nblocks=1, sigma=5, shrinkage_factor=.1,
  
 }
 
-Swithin <- neighborweights::spatial_smoother(cds2,sigma=sigma,nnk=27,stochastic = TRUE)
-Sbetween <- neighborweights::spatial_adjacency(as.matrix(indices),weight_mode="binary", 
-                                               normalize=FALSE,dthresh=bdthresh, include_diagonal=FALSE)/length(perc_dat)
-diag(Sbetween) <- 0
-
-Wg <- Diagonal(x=sqrt(wg))
-
-S <- Wg %*% Swithin %*% Wg
-S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
-S <- S + Sbetween*bw
-S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
+# Swithin <- neighborweights::spatial_smoother(cds2,sigma=sigma,nnk=27,stochastic = TRUE)
+# Sbetween <- neighborweights::spatial_adjacency(as.matrix(indices),weight_mode="binary", 
+#                                                normalize=FALSE,dthresh=bdthresh, include_diagonal=FALSE)/length(perc_dat)
+# diag(Sbetween) <- 0
+# 
+# Wg <- Diagonal(x=sqrt(wg))
+# 
+# S <- Wg %*% Swithin %*% Wg
+# S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
+# S <- S + Sbetween*bw
+# S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
