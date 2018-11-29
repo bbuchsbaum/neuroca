225128637079430ee0ae8aa12533dfd09a488a24
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2018-11-29 09:22:31 -0500

    make spatial_constraints more efficient

--- Inprog/sfpca.R
+++ Inprog/sfpca.R
@@ -7,8 +7,15 @@ prox <- function(X, u, Lu, v, Su, lambda, conv=1e-5) {
   u0 <- u
   crit <- Inf
   while (crit > conv) {
+
     ut <- u + 1/Lu * (X %*% v - Su %*% u)
     ut <- soft_threshold(ut, lambda/Lu)
+    
+    if (sum(ut^2) > 0) {
+      ut <- ut/sqrt(t(ut) %*% Su %*% ut)[1,1]
+    } else {
+      return(ut)
+    }
     crit <- sum( (ut - u)^2)
     u <- ut
   }
@@ -16,22 +23,40 @@ prox <- function(X, u, Lu, v, Su, lambda, conv=1e-5) {
   u
 }
 
-
+#' sparse and smooth functional principal components analysis
+#' 
 #' @param X the data matrix
-#' @param ncomp numbe rof components to estimate
+#' @param ncomp number of components to estimate
 #' @param lu lambda sparsity parameter for the rows
 #' @param lv lambda sparsity parameter for the columns
 #' @param au smoothing parameter for the rows
 #' @param av smoothing parameter for the columns
 #' @param Pu optional penalty matrix for rows
 #' @param Pv optional penalty matrix for columns
-sfpca <- function(X, ncomp=2, lu=1, lv=1, au=1, av=1, coords, Pu=NULL, Pv=NULL, conv=1e-5) {
+#' 
+#'
+#' gen_mat <- function(sd=.2) {
+#'   X <- matrix(0,64,64)
+#'   X[32,32] <- 1
+#'   X[16,16] <- 1
+#'   S <- neighborweights::spatial_smoother(as.matrix(expand.grid(1:64, 1:64)), sigma=8,nnk=80)
+#'   x=S %*% as.vector(X) 
+#'   x + rnorm(length(x), sd=sd)
+#' }
+#' 
+#' X <- replicate(50, gen_mat(.02))
+#' X <- do.call(cbind,X)
+#' X <- t(X)
+#' 
+#' res <- sfpca(X, ncomp=1, av=.5, lv=.5, coords=as.matrix(expand.grid(1:64, 1:64)))
+sfpca <- function(X, ncomp=2, lu=.1, lv=1, au=.1, av=.1, coords, Pu=NULL, Pv=NULL, conv=1e-5) {
 
 
   if (is.null(Pv)) {
-    Pv <- neighborweights::spatial_laplacian(coords, nnk=ncol(coords)^2, weight_mode="binary")
+    Pv <- neighborweights::spatial_laplacian(coords, nnk=9, weight_mode="binary")
   }
   
+
   Sv <- Matrix::Diagonal(n=ncol(X)) + av*Pv
   Lv <- RSpectra::eigs(Sv,1)$values + .01
 
@@ -43,8 +68,8 @@ sfpca <- function(X, ncomp=2, lu=1, lv=1, au=1, av=1, coords, Pu=NULL, Pv=NULL, 
   Lu <- RSpectra::eigs(Su,1)$values + .01
   
   init_svd <- RSpectra::svds(X, 1)
-  v <- init_svd$v
-  u <- init_svd$u
+  v <- init_svd$v[,1]
+  u <- init_svd$u[,1]
   criterion <- Inf
   Xhat <- X
   
@@ -55,10 +80,24 @@ sfpca <- function(X, ncomp=2, lu=1, lv=1, au=1, av=1, coords, Pu=NULL, Pv=NULL, 
 
     while (criterion > conv) {
       u_new <- prox(Xhat, u, Lu, v, Su, lu)
+      while (all(u_new == 0)) {
+        lu <- lu * .8
+        message("new lu: ", lu)
+        u_new <- prox(Xhat, u, Lu, v, Su, lu)
+      }
+      
       v_new <- prox(t(Xhat), v, Lv, u, Sv, lv)
+      while (all(v_new == 0)) {
+        lv <- lv * .8
+        message("new lv: ", lv)
+        v_new <- prox(t(Xhat), v, Lv, u, Sv, lv)
+      }
+      
+      #print(sum(u_new^2))
+      #print(sum(v_new^2))
     
-      v_new <- v_new/sqrt(t(v_new) %*% Sv %*% v_new)[1,1]
-      u_new <- u_new/sqrt(t(u_new) %*% Su %*% u_new)[1,1]
+      #v_new <- v_new/sqrt(t(v_new) %*% Sv %*% v_new)[1,1]
+      #u_new <- u_new/sqrt(t(u_new) %*% Su %*% u_new)[1,1]
     
       criterion <- max(sum( (v_new - v)^2), sum((u_new - u)^2))
       print(criterion)
@@ -69,12 +108,14 @@ sfpca <- function(X, ncomp=2, lu=1, lv=1, au=1, av=1, coords, Pu=NULL, Pv=NULL, 
     v <- v/sum(v^2)
     u <- u/sum(u^2)
     d <- t(u) %*% Xhat %*% v
-    vout[,i] <- v
-    uout[,i] <- u
-    dout[i] <- d
     
-    Xhat = Xhat - d %*% u  %*% t(v)
+    vout[,i] <- as.vector(v)
+    uout[,i] <- as.vector(u)
+    dout[i] <- d[i,1]
+    #Xhat = Xhat - d %*% u  %*% t(v)
+    dout[i] * (u  %*% t(v))
   }
   
-    ##Ou <- 
+  
+  list(d=dout, v=vout, u=uout)
 }
--- R/constraints.R
+++ R/constraints.R
@@ -3,26 +3,70 @@
 #' @param nblocks the number of repeated coordinate blocks
 #' @param sigma the standard deviation of the within-block smoother
 #' @param shrinkage_factor 
-spatial_constraints <- function(coords, nblocks=1, sigma=5, shrinkage_factor=.1, shrinkage_radius=1, 
+spatial_constraints <- function(coords, nblocks=1, 
+                                sigma_within=5, 
+                                sigma_between=1,
+                                shrinkage_factor=.1, 
+                                nnk_within=27,
+                                nnk_between=1,
+                                weight_mode_between="binary",
+                                weight_mode_within="heat",
                                 global_weights=rep(1, ncol(coords)*nblocks)) {
-  cds2 <- do.call(rbind, lapply(nblocks, function(i) cbind(coords, i*50)))
   
-  ##Sw <- neighborweights::spatial_smoother(coords, sigma=sigma, nnk=27, stochastic = TRUE)
-  Swithin <- neighborweights::spatial_smoother(cds2, sigma=sigma, nnk=27, stochastic = TRUE)
-  #Matrix::bdiag(as.list(rep(Sw, nblocks))
+  assert_that(shrinkage_factor > 0 & shrinkage_factor <= 1)
   
-  indices <- rep(1:nrow(coords), nblocks)
+  #cds2 <- do.call(rbind, lapply(1:nblocks, function(i) cbind(coords)))
+  coords <- as.matrix(coords)
   
-  Sbetween <- neighborweights::spatial_adjacency(as.matrix(indices),weight_mode="binary", 
-                                                 normalize=FALSE,dthresh=1, include_diagonal=FALSE)/nblocks
-  diag(Sbetween) <- 0
+  Sw <- neighborweights::spatial_adjacency(coords, sigma=sigma_within, nnk=nnk_within, stochastic = TRUE)
+  #Swithin <- neighborweights::spatial_smoother(cds2, sigma=sigma, nnk=50, stochastic = TRUE)
+  Swithin <- Matrix::bdiag(replicate(nblocks, Sw, simplify=FALSE))
+  #indices <- rep(1:nrow(coords), nblocks)
   
-  Wg <- Diagonal(x=sqrt(global_weights))
+  Sb <- neighborweights::spatial_adjacency(coords,
+                                                 weight_mode="binary", 
+                                                 sigma=sigma_between,
+                                                 normalize=FALSE,
+                                                 nnk=nblocks*nnk_between,
+                                                 dthresh=2.5,
+                                                 include_diagonal=FALSE)
+  Sbt <- as(Sb, "dgTMatrix")
+  nvox <- nrow(coords)
+  offsets <- cumsum(c(0, rep(nvox, nblocks-1)))
   
-  S <- Wg %*% Swithin %*% Wg
-  S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
-  S <- S + Sbetween*bw
-  S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
+  out <- do.call(rbind, lapply(1:nblocks, function(i) {
+    print(i)
+    do.call(rbind, lapply(1:nblocks, function(j) {
+      if (i == j) {
+        NULL
+      } else {
+        cbind(Sbt@i + offsets[i] + 1, Sbt@j + offsets[j] +1, Sbt@x)
+      }
+    }))
+  }))
+  
+  Sbfin <- sparseMatrix(i=out[,1], j=out[,2], x=out[,3], dims=c(nvox*nblocks, nvox*nblocks))
+  Sbfin <- neighborweights::make_doubly_stochastic(Sbfin)
+  
+ 
+  ## scale within matrix by variable weights
+  if (any(global_weights[1] != global_weights)) {
+    Wg <- Diagonal(x=sqrt(global_weights))
+    Swithin <- Wg %*% Swithin %*% Wg
+  }
+  
+  ## compute ration of witin to between weights
+  rat <- sum(Swithin)/sum(Sbfin)
+  sfac <- 1/shrinkage_factor * rat
+  
+  ## balance within and between weights
+  Stot <- (1-shrinkage_factor)*(1/rat)*Swithin + shrinkage_factor*Sbfin
+  
+  ## ratio of 
+  
+  S <- Stot/(RSpectra::eigs_sym(Stot, k=1, which="LA")$values[1])
+  #S <- S + Sbetween*shrinkage_factor
+  #S <- S/(RSpectra::eigs_sym(S, k=1, which="LA")$values[1])
   
  
 }
