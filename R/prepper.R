6e0d7a386523dbf37de2f7e4f50a275d4840adc8
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2019-07-23 08:41:39 -0400

    refactor prepper

--- R/prepper.R
+++ R/prepper.R
@@ -1,3 +1,10 @@
+#' get a fresh pre-processing node cleared of any cached data
+#' @param x the processing pipeline
+#' 
+#' 
+fresh <- function(x,...) UseMethod("fresh")
+
+
 
 #' add a pre-processing stage
 #' @param x the processing pipeline
@@ -47,15 +54,25 @@ prep.prepper <- function(x, X) {
   
 }
 
-fresh.pre_processor <- function(x, preproc=prepper()) {
-  eval(call(class(x)[1]), preproc)
+fresh.prepper <- function(x) {
+  p <- prepper()
+  for (step in x$steps) {
+    p <- prep_node(p, step$name, step$create)
+  }
+  p
 }
 
-prep_node <- function(pipeline, name, forward, reverse, apply, ...) {
+fresh.pre_processor <- function(x, preproc=prepper()) {
+  p <- x$create()
+}
+
+prep_node <- function(pipeline, name, create,  ...) {
+  node <- create()
   ret <- list(name=name,
-              forward=forward,
-              reverse=reverse,
-              apply=apply,
+              create=create,
+              forward=node$forward,
+              reverse=node$reverse,
+              apply=node$apply,
               ...)
   class(ret) <- c(name, "pre_processor")
   add_node(pipeline, ret)
@@ -63,180 +80,206 @@ prep_node <- function(pipeline, name, forward, reverse, apply, ...) {
 
 #' @export
 pass <- function(preproc=prepper()) {
-  forward <- function(X, colind) {
-    X
-  }
   
-  reverse <- function(X, colind) {
-    X
-  }
+  create <- function() {
+    list(
+      forward = function(X, colind=NULL) {
+        if (is.null(colind)) X else X[,colind,drop=FALSE]
+      },
   
-  apply <- function(X, colind) {
-    X
-  }
+      reverse = function(X, colind=NULL) {
+        if (is.null(colind)) X else X[,colind,drop=FALSE]
+      },
   
-  prep_node(preproc, "pass", forward, reverse, apply)
-  
-}
-
-#' @export
-center <- function(preproc=prepper()) {
-  env=new.env()
-  
-  forward <- function(X) {
-    cmeans <- colMeans(X)
-    env[["cmeans"]] <- cmeans
-    sweep(X, 2, cmeans, "-")
-  }
-  
-  apply <- function(X, colind=NULL) {
-    cmeans <- env[["cmeans"]] 
-    if (is.null(colind)) {
-      sweep(X, 2, cmeans, "-")
-    } else {
-      assert_that(ncol(X) == length(colind))
-      sweep(X, 2, cmeans[colind], "-")
-    }
-  }
-    
-  reverse=function(X, colind=NULL) {
-    if (is.null(colind)) {
-      sweep(X, 2, env[["cmeans"]], "+")
-    } else {
-      assert_that(ncol(X) == length(colind))
-      sweep(X, 2, env[["cmeans"]][colind], "+")
-    }
-  }
-  
-  prep_node(preproc, "center", forward, reverse, apply)
-}
-
-#' @export
-colscale <- function(preproc=prepper(), type=c("unit", "z", "weights"), weights=NULL) {
-  type <- match.arg(type)
-  
-  if (type != "weights") {
-    warning("colscale: weight ignored because type != 'weights'")
-  }
-  if (type == "weights") {
-    assert_that(!is.null(weights))
-  }
-  
-  env=new.env()
-  
-  forward <- function(X) {
-    wts <- if (type == "weights") {
-      assert_that(length(weights) == ncol(X))
-      weights
-    } else {
-      sds <- matrixStats::colSds(X)
-    
-      if (type == "unit") {
-        sds <- sds * sqrt(nrow(X)-1)
+      apply = function(X, colind=NULL) {
+        if (is.null(colind)) X else X[,colind,drop=FALSE]
       }
-      
-      sds[sds == 0] <- median(sds)
-      1/sds
-    }
-    env[["weights"]] <- wts
-    sweep(X, 2, wts, "*")
-    
+    )
   }
   
-  apply <- function(X, colind=NULL) {
-    if (is.null(colind)) {
-      sweep(X, 2, env[["weights"]], "*")
-    } else {
-      assert_that(ncol(X) == length(colind))
-      sweep(X, 2, env[["weights"]][colind], "*")
-    }
-  }
+  prep_node(preproc, "pass", create)
   
-  reverse=function(X, colind=NULL) {
-    if (is.null(colind)) {
-      sweep(X, 2, env[["weights"]], "/")
-    } else {
-      assert_that(ncol(X) == length(colind))
-      sweep(X, 2, env[["weights"]][colind], "/")
-    }
-  }
-  
-  prep_node(preproc, "colscale", forward, reverse, apply)
 }
+
+#' @export
+center <- function(preproc = prepper()) {
+  create <- function() {
+    env = new.env()
+    
+    list(
+      forward = function(X) {
+        cmeans <- colMeans(X)
+        env[["cmeans"]] <- cmeans
+        sweep(X, 2, cmeans, "-")
+      },
+      
+      apply = function(X, colind = NULL) {
+        cmeans <- env[["cmeans"]]
+        if (is.null(colind)) {
+          sweep(X, 2, cmeans, "-")
+        } else {
+          assert_that(ncol(X) == length(colind))
+          sweep(X, 2, cmeans[colind], "-")
+        }
+      },
+      
+      reverse = function(X, colind = NULL) {
+        if (is.null(colind)) {
+          sweep(X, 2, env[["cmeans"]], "+")
+        } else {
+          assert_that(ncol(X) == length(colind))
+          sweep(X, 2, env[["cmeans"]][colind], "+")
+        }
+      }
+    )
+  }
+  
+  prep_node(preproc, "center", create)
+}
+
+#' @export
+colscale <- function(preproc = prepper(),
+           type = c("unit", "z", "weights"),
+           weights = NULL) {
+    type <- match.arg(type)
+    
+    if (type != "weights" && !is.null(weights)) {
+      warning("colscale: weight ignored because type != 'weights'")
+    }
+    if (type == "weights") {
+      assert_that(!is.null(weights))
+    }
+    
+    create <- function() {
+      env = new.env()
+      list(
+        forward = function(X) {
+          wts <- if (type == "weights") {
+            assert_that(length(weights) == ncol(X))
+            weights
+          } else {
+            sds <- matrixStats::colSds(X)
+            
+            if (type == "unit") {
+              sds <- sds * sqrt(nrow(X) - 1)
+            }
+            
+            sds[sds == 0] <- median(sds)
+            1 / sds
+          }
+          env[["weights"]] <- wts
+          sweep(X, 2, wts, "*")
+          
+        },
+        
+        apply = function(X, colind = NULL) {
+          if (is.null(colind)) {
+            sweep(X, 2, env[["weights"]], "*")
+          } else {
+            assert_that(ncol(X) == length(colind))
+            sweep(X, 2, env[["weights"]][colind], "*")
+          }
+        },
+        
+        reverse = function(X, colind = NULL) {
+          if (is.null(colind)) {
+            sweep(X, 2, env[["weights"]], "/")
+          } else {
+            assert_that(ncol(X) == length(colind))
+            sweep(X, 2, env[["weights"]][colind], "/")
+          }
+        }
+      )
+    }
+    
+    prep_node(preproc, "colscale", create)
+  }
 
 #' dimension reduction as a pre-processing stage
 #' @param preproc the pipeline
 #' @param method the dimension reduction method (e.g. pca)
 #' @param ... args to be passed to the dimension reduction method
 #' @export
-dim_reduce <- function(preproc=prepper(), method=pca, ...) {
-  env=new.env()
+dim_reduce <- function(preproc = prepper(),
+                       method = pca,
+                       ...) {
   args <- list(...)
   
-  forward <- function(X) {
-    fit <- do.call(method, append(list(X), args))
-    env[["fit"]] <- fit
-    env[["X"]] <- X
-    scores(fit)
+  create = function() {
+    env = new.env()
+    
+    list(
+      forward = function(X) {
+        fit <- do.call(method, append(list(X), args))
+        env[["fit"]] <- fit
+        env[["X"]] <- X
+        scores(fit)
+      },
+      
+      apply = function(X, colind = NULL) {
+        if (is.null(colind)) {
+          project(env[["fit"]], X)
+        } else {
+          assert_that(ncol(X) == length(colind))
+          project(env[["fit"]], X, colind = colind)
+        }
+      },
+      
+      reverse = function(X, colind = NULL) {
+        if (is.null(colind)) {
+          env[["X"]]
+        } else {
+          assert_that(ncol(X) == length(colind))
+          env[["X"]][, colind, drop = FALSE]
+        }
+      }
+    )
   }
   
-  apply <- function(X, colind=NULL) {
-    if (is.null(colind)) {
-      project(env[["fit"]], X)
-    } else {
-      assert_that(ncol(X) == length(colind))
-      project(env[["fit"]], X, colind=colind)
-    }
-  }
-  
-  reverse=function(X, colind=NULL) {
-    if (is.null(colind)) {
-      env[["X"]]
-    } else {
-      assert_that(ncol(X) == length(colind))
-      env[["X"]][,colind,drop=FALSE]
-    }
-  }
-  
-  prep_node(preproc, "dim_reduce", forward, reverse, apply)
+  prep_node(preproc, "dim_reduce", create)
 }
 
 
 
+
 #' @export
-standardize <- function(preproc=prepper()) {
-  env=new.env()
-  
-  forward <- function(X) {
-    sds <- matrixStats::colSds(X)
-    sds[sds == 0] <- median(sds)
-    cmeans <- colMeans(X)
-    env[["sds"]] <- sds
-    env[["cmeans"]] <- cmeans
-    x1 <- sweep(X, 2, cmeans, "-")
-    sweep(x1, 2, sds, "/")
+standardize <- function(preproc = prepper()) {
+  create <- function() {
+    env = new.env()
+    
+    list(
+      forward = function(X) {
+        sds <- matrixStats::colSds(X)
+        sds[sds == 0] <- median(sds)
+        cmeans <- colMeans(X)
+        env[["sds"]] <- sds
+        env[["cmeans"]] <- cmeans
+        x1 <- sweep(X, 2, cmeans, "-")
+        sweep(x1, 2, sds, "/")
+      },
+      
+      apply = function(X, colind = NULL) {
+        if (is.null(colind)) {
+          sweep(X, 2, env[["sds"]], "/")
+        } else {
+          assert_that(ncol(X) == length(colind))
+          x1 <- sweep(X, 2, env[["cmeans"]][colind], "-")
+          sweep(x1, 2, env[["sds"]][colind], "/")
+        }
+      },
+      
+      reverse = function(X, colind = NULL) {
+        if (is.null(colind)) {
+          x0 <- sweep(X, 2, env[["sds"]], "*")
+          sweep(x0, 2, env[["cmeans"]], "+")
+        } else {
+          assert_that(ncol(X) == length(colind))
+          x0 <- sweep(X, 2, env[["sds"]][colind], "*")
+          sweep(x0, 2, env[["cmeans"]][colind], "+")
+        }
+      }
+    )
   }
-  
-  apply <- function(X, colind=NULL) {
-    if (is.null(colind)) {
-      sweep(X, 2, env[["sds"]], "/")
-    } else {
-      assert_that(ncol(X) == length(colind))
-      x1 <- sweep(X, 2, env[["cmeans"]][colind], "-")
-      sweep(x1, 2, env[["sds"]][colind], "/")
-    }
-  }
-  
-  reverse=function(X, colind=NULL) {
-    if (is.null(colind)) {
-      x0 <- sweep(X, 2, env[["sds"]], "*")
-      sweep(x0, 2, env[["cmeans"]], "+")
-    } else {
-      assert_that(ncol(X) == length(colind))
-      x0 <- sweep(X, 2, env[["sds"]][colind], "*")
-      sweep(x0, 2, env[["cmeans"]][colind], "+")
-    }
-  }
-  prep_node(preproc, "standardize", forward, reverse, apply)
+  prep_node(preproc, "standardize", create)
 }
 
